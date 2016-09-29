d8331af51065a2b40bcb59aef830bfdc474d3d8a
Matias Eyzaguirre <matias@fortlogic.net>
2016-09-28 20:29:46 -0600

    basic testing framework setup.

---
+++ src/test/clash/TestsMain.hs
@@ -0,0 +1,14 @@
+module Main where
+
+import Test.Hspec
+
+import CLaSHTests
+
+main = hspec $ do
+  clashTests "CLaSH Tests"
+  describe "CLaSH Testbenches" $ do
+    it "should run hardware testbenches in Haskell" $ do
+      pendingWith "waiting on testbenches"
+  describe "VHDL Testbenches" $ do
+    it "should run hardware testbenches in GHDL" $ do
+      pendingWith "Waiting on testbenches and GHDL infrastructure"
