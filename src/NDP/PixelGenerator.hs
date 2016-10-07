0099b07a74500c808e1e8a536152a0eac887af0c
Matias Eyzaguirre <matias@fortlogic.net>
2016-10-07 11:18:03 -0600

    Started putting together the pieces to form the HDMI signal generator.

--- src/NDP/PixelGenerator.hs
+++ src/NDP/PixelGenerator.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE Arrows #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TemplateHaskell #-}
-module VideoGenerator where
+module NDP.PixelGenerator (PixelCoord (),
+                           pixelCounter,
+                           pixelControl,
+                           pixelGenerator) where
 
 import CLaSH.Prelude
 import CLaSH.Prelude.Explicit
@@ -9,9 +12,9 @@ import qualified Prelude as P
 
 import NDP.CBMColor
 import NDP.Clocking
-import NDP.TMDS
 import NDP.VideoTiming
 
+-- (Row, Column) or (y,x)
 data PixelCoord = Px (Unsigned 10) (Unsigned 10)
                 deriving (Show, Eq)
 
@@ -31,33 +34,12 @@ pixelControl t = (maybeCoord,
                      then Just $ Px vT (resize hT)
                      else Nothing
 
-tmdsLow :: TMDS
-tmdsLow = TMDSControl 0
+staticPixelGenerator :: PixelCoord -> RGBColor
+staticPixelGenerator (Px y x) = if inFrameX && inFrameY
+                                then cbmToRGB Blue
+                                else cbmToRGB LightBlue
+  where inFrameY = (y >= 44) && (y < 556)
+        inFrameX = (x >= 144) && (x < 656)
 
-rgb2tmds :: Maybe RGBColor -> -- color
-            Bit ->            -- hsync
-            Bit ->            -- vsync
-            Vec 3 TMDS        -- R G B
-rgb2tmds Nothing h v = tmdsLow :> tmdsLow :> TMDSControl (v ++# h) :> Nil
-rgb2tmds (Just (RGB r g b)) _ _ = r' :> g' :> b' :> Nil
-  where r' = TMDSData r
-        g' = TMDSData g
-        b' = TMDSData b
-
-rgb2Video :: SignalPx (Maybe RGBColor) ->
-             SignalPx Bit ->
-             SignalPx Bit ->
-             SignalPx (Vec 3 (BitVector 10))
-rgb2Video mc hSync vSync = bundle' pxClk (tmdsR :> tmdsG :> tmdsB :> Nil)
-  where tmdsVec = unbundle' pxClk $ rgb2tmds <$> mc <*> hSync <*> vSync
-        tmdsR = tmdsEncoder $ tmdsVec !! 0
-        tmdsG = tmdsEncoder $ tmdsVec !! 1
-        tmdsB = tmdsEncoder $ tmdsVec !! 2
-
-generateVideo :: (SignalPx (Maybe PixelCoord) -> SignalPx (Maybe RGBColor)) ->
-                 SignalPx (Vec 3 (BitVector 10))
-generateVideo gen = rgb2Video rgb hSync vSync
-  where timer = pixelCounter
-        (maybeCoord, hSync, vSync) = unbundle' pxClk  $ pixelControl <$> timer
-        rgb = gen maybeCoord
- 
+pixelGenerator :: SignalPx (Maybe PixelCoord) -> SignalPx (Maybe RGBColor)
+pixelGenerator coord = (staticPixelGenerator <$>) <$> coord
