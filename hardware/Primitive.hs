c0cbbcf4b4c0b480762304b6981f8678223356ba
Matias Eyzaguirre <matias@fortlogic.net>
2018-05-11 16:06:26 -0600

    Updated resolver.

--- hardware/Primitive.hs
+++ hardware/Primitive.hs
@@ -3,12 +3,12 @@ module Primitive ( HDL (..)
 
 import Clash.Annotations.Primitive
 import Paths_NDP
-import System.IO.Unsafe
 import System.FilePath
+import System.IO.Unsafe
 
 ndp_primitive :: HDL -> String -> Primitive
 ndp_primitive hdl name = Primitive hdl $ unsafePerformIO (getDataFileName path)
-  where path = "hardware" </> "hdl" </> hdlName hdl </> name <.> "json"
+  where path = "hardware" </> "Primitive" </> hdlName hdl </> name <.> "json"
         hdlName VHDL = "vhdl"
         hdlName Verilog = "verilog"
         hdlName SystemVerilog = "system-verilog"
