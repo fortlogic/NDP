3de2bf08e652653a078b57575fd2190030f91a20
Matias Eyzaguirre <matias@fortlogic.net>
2018-10-01 17:57:27 -0600

    just for fun
    gource is fun to play with.

---
+++ vanity.sh
@@ -0,0 +1,1 @@
+gource --seconds-per-day 1 --max-file-lag 0.5 --camera-mode overview --auto-skip-seconds 0.5 --key --title "NDP" -o - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -pix_fmt yuv420p -crf 1 -threads 4 -bf 0 gource.mp4
