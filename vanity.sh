63f21d8fee8490ef8fef8ca7ddaf73ec19689088
Matias Eyzaguirre <matias@fortlogic.net>
2018-10-04 15:26:39 -0600

    removed md5 dependency.

--- vanity.sh
+++ vanity.sh
@@ -1,5 +1,5 @@
 gource --seconds-per-day 1 --max-file-lag 0.5 --camera-mode overview \
        --auto-skip-seconds 0.5 --key --title "NDP" -o - \
 | \
-ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset placebo \
-       -pix_fmt yuv420p -crf 23 -threads 1 -bf 0 gource.mp4
+ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset veryslow \
+       -pix_fmt yuv420p -crf 23 -threads 8 -bf 0 $1
