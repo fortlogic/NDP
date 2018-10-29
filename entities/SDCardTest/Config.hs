71124fc103d31fcb5d5668c215cb66c793a9fcd6
Matias Eyzaguirre <matias@fortlogic.net>
2018-10-29 15:41:04 -0600

    remove SD card entity
    doesn’t need to exist yet.

--- entities/SDCardTest/Config.hs
+++
@@ -1,8 +0,0 @@
-module TopLevel.SDCardTest.Config where
-
-constraints = [ "requiredConstraints",
-                "builtinLEDNets",
-                "builtinButtonNets",
-                "sdCardSpiNets" ]
-
-clock = "dumb_clock"
