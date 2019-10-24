f418fd2a3dc649790c39c28e911fdae67f19cccd
Matias Eyzaguirre <matias@fortlogic.net>
2019-10-24 13:40:24 -0600

    Separating synthesis domains
    Template Haskell doesn’t seem to like having a domain be defined in the same file that the domain it’s derived from is defined.
    
    Signed-off-by: Matias Eyzaguirre <matias@fortlogic.net>

---
+++ hardware/NDP/Clocking/Domains/Pixel.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+module NDP.Clocking.Domains.Pixel ( Pixel
+                                  , vPixel ) where
+
+import Clash.Prelude
+
+import NDP.Clocking.Domains.Builtin
+
+-- Pixel clock is 40MHz
+createDomain vBuiltin
+  { vName          = "Pixel"
+  , vPeriod        = 25000 -- 40MHz
+  }
